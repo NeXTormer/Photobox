d39792e35c79bc449611e6971f1e1abad89722f2
Felix Holz <felix.holz@me.com>
2018-02-25 21:54:42 +0100

    working on arduino

--- Arduino/photobox/photobox.ino
+++ Arduino/photobox/photobox.ino
@@ -1,29 +1,60 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(7, INPUT);
-  pinMode(8, INPUT);
+#define buttoncount 3
 
+const int buttons[3] = { 4, 5, 6 };
+const int ledPin = 7;
+
+
+unsigned long lastDebounceTime[buttoncount];
+unsigned long debounceDelay = 50;
+
+int ledState = HIGH;
+
+int buttonState[buttoncount];
+int lastButtonState[buttoncount];
+
+void setup() 
+{
   Serial.begin(9600);
+
+  for(int i = 0; i < buttoncount; i++)
+  {
+        lastButtonState[i] = LOW;
+        lastDebounceTime[i] = 0;
+
+        pinMode(buttons[i], INPUT);
+  }
+  
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, ledState);
 }
 
-bool last7 = true;
-bool last8 = true;
-
-void loop() {
-
-  bool current7 = digitalRead(7);
-  bool current8 = digitalRead(8);
-
-  if(last7 != current7)
+void loop() 
+{
+  for(int i = 0; i < buttoncount; i++)
   {
-    Serial.println("7");
-  }
-  if(last8 != current8)
-  {
-    Serial.println("8");
-  }
+    int reading = digitalRead(buttons[i]);
+    if (reading != lastButtonState[i]) 
+    {
+      lastDebounceTime[i] = millis();
+    }
 
-  last8 = current8;
-  last7 = current7;
-  
+    if ((millis() - lastDebounceTime[i]) > debounceDelay) 
+    {
+      if (reading != buttonState[i]) 
+      {
+        buttonState[i] = reading;
+        if (buttonState[i] == HIGH) 
+        {
+          //button pressed
+          Serial.print(i);
+          ledState = !ledState;
+        }
+      }
+    }
+    
+    digitalWrite(ledPin, ledState);
+    
+    lastButtonState[i] = reading;
+  }
 }
