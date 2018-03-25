1e392ac1f5b7bd40bbe7c9ffaecdd0801ca9cbd5
Felix Holz <felix.holz@me.com>
2018-03-25 14:40:22 +0200

    new arduino sketch

--- Arduino/photobox/photobox.ino
+++ Arduino/photobox/photobox.ino
@@ -4,12 +4,21 @@ ButtonDebounce b1(4, 100);
 ButtonDebounce b2(5, 100);
 ButtonDebounce b3(6, 100);
 
+bool red = false;
+bool green = false;
+
+const int r1 = 6;
+const int r2 = 12;
+const int g1 = 2;
+const int g2 = 13;
+
 
 void b1Callback(int state)
 {
   if(state)
   {
     Serial.println("Button 1"); 
+    green = !green;
   }
 }
 
@@ -18,6 +27,7 @@ void b2Callback(int state)
   if(state)
   {
     Serial.println("Button 2"); 
+  	red = !red;
   }
 }
 
@@ -36,6 +46,13 @@ void setup()
   b1.setCallback(b1Callback);
   b2.setCallback(b2Callback);
   b3.setCallback(b3Callback);
+
+
+  pinMode(r1, OUTPUT);
+  pinMode(r2, OUTPUT);
+  pinMode(g1, OUTPUT);
+  pinMode(g2, OUTPUT);
+
 }
 
 void loop() 
@@ -43,4 +60,10 @@ void loop()
   b1.update();
   b2.update();
   b3.update();
+
+  digitalWrite(r1, red);
+  digitalWrite(r2, red);
+  digitalWrite(g1, green);
+  digitalWrite(g2, green);
+   
 }
